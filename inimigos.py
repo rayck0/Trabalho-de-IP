8a1089f79fb460982c20100d1905c42f7f208398
Gustavo Queiroz <gjaq@cin.ufpe.br>
2025-12-17 12:51:14 -0300

    Refactor enemy classes and loading mechanism

--- inimigos.py
+++ inimigos.py
@@ -1,101 +1,96 @@
 import pygame
 import os
+from config import *
 
-#diretorios
-diretorio_principal = os.path.dirname(__file__)
-diretorio_imagens = os.path.join(diretorio_principal, 'Sprites')
+DIRETORIO = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Sprites')
 
-#robo voador-----------------------------------------------------------------------------------------------------
-inimigo_RoboVoador = pygame.image.load(os.path.join(diretorio_imagens, 'enemy_roboVoador.png')).convert_alpha()
-class Robo_voador(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
+# Carregamento
+sheet_voador = pygame.image.load(os.path.join(DIRETORIO, 'enemy_roboVoador.png')).convert_alpha()
+sheet_dragao = pygame.image.load(os.path.join(DIRETORIO, 'enemy_roboDragao.png')).convert_alpha()
+sheet_cobra  = pygame.image.load(os.path.join(DIRETORIO, 'enemy_roboCobra.png')).convert_alpha()
+sheet_bola   = pygame.image.load(os.path.join(DIRETORIO, 'enemy_roboBola.png')).convert_alpha()
+sheet_zangao = pygame.image.load(os.path.join(DIRETORIO, 'enemy_roboZangao.png')).convert_alpha()
+
+class InimigoPadrao(pygame.sprite.Sprite):
+    def __init__(self, pos, grupos, sheet, frames, larg_orig, alt_orig, tam_final, velocidade, vida):
+        super().__init__(grupos)
         self.sprites = []
-        for x in range(8): #8 frames (88x64)
-            img = inimigo_RoboVoador.subsurface((88*x, 64), (88,64))
-            #caso precise aumentar o tamanho --------------
-            #img = pygame.transform.scale(img, (88*, 64*))
-            self.sprites.append(img)
-        self.index = 0
-        self.image = self.sprites[self.index]
-    def update(self):
-        if self.index > 7:
-            self.index = 0
-        self.index += 0.25
-        self.image = self.sprites[int(self.index)]
-
-#robo dragão----------------------------------------------------------------------------------------------------
-inimigo_RoboDragao = pygame.image.load(os.path.join(diretorio_imagens, 'enemy_roboDragao.png')).convert_alpha()
-class Robo_dragao(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.sprites = []
-        for x in range(6): #6 frames (256x256)
-            img = inimigo_RoboVoador.subsurface((256*x, 256), (256,256))
-            #caso precise aumentar o tamanho --------------
-            #img = pygame.transform.scale(img, (256*, 256*))
-            self.sprites.append(img)
-        self.index = 0
-        self.image = self.sprites[self.index]
-    def update(self):
-        if self.index > 5:
-            self.index = 0
-        self.index += 0.20
-        self.image = self.sprites[int(self.index)]
         
-#robo cobra----------------------------------------------------------------------------------------------------
-inimigo_RoboCobra = pygame.image.load(os.path.join(diretorio_imagens, 'enemy_roboCobra.png')).convert_alpha()
-class Robo_cobra(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.sprites = []
-        for x in range(5): #5 frames (64x64)
-            img = inimigo_RoboVoador.subsurface((64*x, 64), (64,64))
-            #caso precise aumentar o tamanho --------------
-            #img = pygame.transform.scale(img, (64*, 64*))
-            self.sprites.append(img)
+        # Recorte e Escala
+        for x in range(frames):
+            corte = sheet.subsurface((x * larg_orig, 0), (larg_orig, alt_orig))
+            img_redim = pygame.transform.scale(corte, (tam_final, tam_final))
+            self.sprites.append(img_redim)
+            
         self.index = 0
         self.image = self.sprites[self.index]
-    def update(self):
-        if self.index > 4:
-            self.index = 0
-        self.index += 0.20
-        self.image = self.sprites[int(self.index)]
+        self.rect = self.image.get_rect(topleft=pos)
+        
+        # Física e Vida
+        self.pos_real = pygame.math.Vector2(self.rect.topleft)
+        self.velocidade = velocidade
+        self.vida = vida
+        self.vida_maxima = vida # Útil se quisermos desenhar barra de vida dps
+        
+        # Controle do Pisca-Pisca flash do dano
+        self.hit_timer = 0 
 
-#robo bola-----------------------------------------------------------------------------------------------------
-inimigo_RoboBola = pygame.image.load(os.path.join(diretorio_imagens, 'enemy_roboBola.png')).convert_alpha()
-class Robo_bola(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.sprites = []
-        for x in range(4): #4 frames (64x64)
-            img = inimigo_RoboVoador.subsurface((64*x, 64), (64,64))
-            #caso precise aumentar o tamanho --------------
-            #img = pygame.transform.scale(img, (64*, 64*))
-            self.sprites.append(img)
-        self.index = 0
-        self.image = self.sprites[self.index]
-    def update(self):
-        if self.index > 3:
-            self.index = 0
-        self.index += 0.15
-        self.image = self.sprites[int(self.index)]
+    def cacar_jogador(self, player):
+        direcao = player.PosicaoReal - self.pos_real
+        if direcao.magnitude() > 0:
+            direcao = direcao.normalize()
+            self.pos_real += direcao * self.velocidade
+            self.rect.center = round(self.pos_real.x), round(self.pos_real.y)
 
-#robo Zangão----------------------------------------------------------------------------------------------------
-inimigo_RoboZangao = pygame.image.load(os.path.join(diretorio_imagens, 'enemy_roboZangao.png')).convert_alpha()
-class Robo_zangao(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.sprites = []
-        for x in range(4): #4 frames (64x64)
-            img = inimigo_RoboVoador.subsurface((64*x, 64), (64,64))
-            #caso precise aumentar o tamanho --------------
-            #img = pygame.transform.scale(img, (64*, 64*))
-            self.sprites.append(img)
-        self.index = 0
-        self.image = self.sprites[self.index]
+    def receber_dano(self, quantidade):
+        # Reduz a vida e ativa o flash branco
+        self.vida -= quantidade
+        self.hit_timer = 5 # Fica branco por 5 frames
+        
+        if self.vida <= 0:
+            self.kill() # Remove do jogo
+            return True # Retorna Verdadeiro (Morreu)
+        return False # Retorna Falso (Ainda vivo)
+
     def update(self):
-        if self.index > 3:
-            self.index = 0
-        self.index += 0.15
-        self.image = self.sprites[int(self.index)]
+        # Animação Normal
+        self.index += 0.2
+        if self.index >= len(self.sprites): self.index = 0
+        
+        # Pega a imagem original do frame atual
+        imagem_atual = self.sprites[int(self.index)]
+        
+        # Efeito de Dano de piscar
+        if self.hit_timer > 0:
+            self.hit_timer -= 1
+            # Cria uma cópia para não estragar a original
+            imagem_flash = imagem_atual.copy()
+            # Pinta de branco (BLEND_ADD soma cor, branco + cor = muito branco)
+            imagem_flash.fill((255, 255, 255), special_flags=pygame.BLEND_ADD)
+            self.image = imagem_flash
+        else:
+            # Se não tomou dano, mostra a imagem normal
+            self.image = imagem_atual
+
+# CONFIGURAÇÃO DE VIDA (HP)
+# Player dá 10 de dano por tiro 
+
+class RoboVoador(InimigoPadrao): # Morre em 2 tiros
+    def __init__(self, pos, grupos):
+        super().__init__(pos, grupos, sheet_voador, 8, 88, 64, 64, velocidade=3.5, vida=20)
+
+class RoboBola(InimigoPadrao): # Morre em 3 tiros
+    def __init__(self, pos, grupos):
+        super().__init__(pos, grupos, sheet_bola, 4, 64, 64, 64, velocidade=2.5, vida=30)
+
+class RoboZangao(InimigoPadrao): # Morre em 3 tiros
+    def __init__(self, pos, grupos):
+        super().__init__(pos, grupos, sheet_zangao, 4, 64, 64, 64, velocidade=3.0, vida=30)
+
+class RoboCobra(InimigoPadrao): # Morre em 5 tiros, Tanque
+    def __init__(self, pos, grupos):
+        super().__init__(pos, grupos, sheet_cobra, 5, 64, 64, 64, velocidade=2.0, vida=50)
+
+class RoboDragao(InimigoPadrao): # BOSS: Morre em 50 tiros
+    def __init__(self, pos, grupos):
+        super().__init__(pos, grupos, sheet_dragao, 6, 256, 256, 190, velocidade=1.5, vida=500)
